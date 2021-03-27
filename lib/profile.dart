8d07cc1fa10fdaec1bd6df5df635abdde096dc2b
Yodra Muhamad Akbar N <56280668+yodraMuhammad@users.noreply.github.com>
2021-03-27 09:10:16 +0700

    push

---
+++ lib/profile.dart
@@ -0,0 +1,249 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  String username, name, email, nomer;
+
+  Widget _buildFoto() {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      child: Image.asset('assets/img/profile.png'),
+    );
+  }
+
+  Widget _buildBtnChange() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          height: 1.2 * (MediaQuery.of(context).size.height / 30),
+          width: 6 * (MediaQuery.of(context).size.width / 20),
+          margin: EdgeInsets.only(bottom: 30),
+          child: RaisedButton(
+            elevation: 5.0,
+            color: Colors.red,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            onPressed: () {},
+            child: Text(
+              "Change Photo",
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 1.5,
+                fontSize: 10,
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildUsernameRow() {
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: TextFormField(
+        keyboardType: TextInputType.emailAddress,
+        onChanged: (value) {
+          setState(() {
+            username = value;
+          });
+        },
+        decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            prefixIcon: Icon(
+              Icons.alternate_email,
+              color: Color(0xFFE72020),
+            ),
+            labelText: 'Username'),
+      ),
+    );
+  }
+
+  Widget _buildEmailRow() {
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: TextFormField(
+        keyboardType: TextInputType.text,
+        onChanged: (value) {
+          setState(() {
+            email = value;
+          });
+        },
+        decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            prefixIcon: Icon(
+              Icons.email_outlined,
+              color: Colors.red,
+            ),
+            labelText: 'Email'),
+      ),
+    );
+  }
+
+  Widget _buildNameRow() {
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: TextFormField(
+        keyboardType: TextInputType.emailAddress,
+        onChanged: (value) {
+          setState(() {
+            name = value;
+          });
+        },
+        decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            prefixIcon: Icon(
+              Icons.person,
+              color: Color(0xFFE72020),
+            ),
+            labelText: 'Name'),
+      ),
+    );
+  }
+
+  Widget _buildNomerRow() {
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: TextFormField(
+        keyboardType: TextInputType.emailAddress,
+        onChanged: (value) {
+          setState(() {
+            nomer = value;
+          });
+        },
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+          prefixIcon: Icon(
+            Icons.local_phone,
+            color: Color(0xFFE72020),
+          ),
+          labelText: 'No. Handphone',
+        ),
+      ),
+    );
+  }
+
+  Widget _buildButton() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          height: 1.2 * (MediaQuery.of(context).size.height / 20),
+          width: 6 * (MediaQuery.of(context).size.width / 7),
+          margin: EdgeInsets.only(top: 40),
+          child: RaisedButton(
+            elevation: 5.0,
+            color: Colors.red,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(40.0),
+            ),
+            onPressed: () {},
+            child: Text(
+              "Save",
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 1.5,
+                fontSize: MediaQuery.of(context).size.height / 40,
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildContainer() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ClipRRect(
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.8,
+            width: MediaQuery.of(context).size.width * 0.9,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3), // changes position of shadow
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "EDIT PROFILE",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                _buildFoto(),
+                _buildBtnChange(),
+                _buildUsernameRow(),
+                _buildEmailRow(),
+                _buildNameRow(),
+                _buildNomerRow(),
+                _buildButton(),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Color(0xFFE72020),
+          title: Text(
+            'PROFILE',
+          ),
+          leading: GestureDetector(
+            onTap: () {/* Write listener code here */},
+            child: Icon(
+              Icons.arrow_back_ios, // add custom icons also
+            ),
+          ),
+        ),
+        body: Stack(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _buildContainer(),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/registrasi.dart
@@ -0,0 +1,259 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  String email, password, username;
+  bool agree = false;
+
+  Widget _buildLogo() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 40, bottom: 50),
+          child: Image.asset('assets/img/logo2.png'),
+        )
+      ],
+    );
+  }
+
+  Widget _buildUsernameRow() {
+    return Padding(
+      padding: EdgeInsets.only(right: 20, left: 20, bottom: 10),
+      child: TextFormField(
+        keyboardType: TextInputType.emailAddress,
+        onChanged: (value) {
+          setState(() {
+            username = value;
+          });
+        },
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(5),
+          ),
+          labelText: 'Username',
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEmailRow() {
+    return Padding(
+      padding: EdgeInsets.only(right: 20, left: 20, bottom: 10),
+      child: TextFormField(
+        keyboardType: TextInputType.text,
+        onChanged: (value) {
+          setState(() {
+            email = value;
+          });
+        },
+        decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5),
+            ),
+            labelText: 'Email'),
+      ),
+    );
+  }
+
+  Widget _buildPasswordRow() {
+    return Padding(
+      padding: EdgeInsets.only(right: 20, left: 20, bottom: 10),
+      child: TextFormField(
+        keyboardType: TextInputType.text,
+        onChanged: (value) {
+          setState(() {
+            password = value;
+          });
+        },
+        decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5),
+            ),
+            labelText: 'Password'),
+      ),
+    );
+  }
+
+  Widget _buildRePasswordRow() {
+    return Padding(
+      padding: EdgeInsets.only(right: 20, left: 20, bottom: 10),
+      child: TextFormField(
+        keyboardType: TextInputType.text,
+        onChanged: (value) {
+          setState(() {
+            password = value;
+          });
+        },
+        decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5),
+            ),
+            labelText: 'Re - Password'),
+      ),
+    );
+  }
+
+  Widget _buildCheckBox() {
+    return Row(
+      children: [
+        Material(
+          child: Checkbox(
+            value: agree,
+            onChanged: (value) {
+              setState(() {
+                agree = value;
+              });
+            },
+          ),
+        ),
+        Text(
+          'I have read and accept terms and conditions',
+          style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+        )
+      ],
+    );
+  }
+
+  Widget _buildLoginButton() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          height: 1.2 * (MediaQuery.of(context).size.height / 20),
+          width: 6 * (MediaQuery.of(context).size.width / 8),
+          margin: EdgeInsets.only(bottom: 0),
+          child: RaisedButton(
+            elevation: 5.0,
+            color: Colors.red,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            onPressed: () {},
+            child: Text(
+              "Login",
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 1.5,
+                fontSize: MediaQuery.of(context).size.height / 40,
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildHaveAcc() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "Already have Account ?",
+          style: TextStyle(
+            fontSize: 12,
+          ),
+        ),
+        TextButton(
+          onPressed: () {},
+          child: Text(
+            'Login',
+            style: TextStyle(
+              color: Color(0xFFE72020),
+              fontSize: 12,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildContainer() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ClipRRect(
+          borderRadius: BorderRadius.all(
+            Radius.circular(9),
+          ),
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.62,
+            width: MediaQuery.of(context).size.width * 0.85,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3), // changes position of shadow
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(top: 20, bottom: 20),
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  child: Text(
+                    "REGISTER",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                _buildUsernameRow(),
+                _buildEmailRow(),
+                _buildPasswordRow(),
+                _buildRePasswordRow(),
+                _buildCheckBox(),
+                _buildLoginButton(),
+                _buildHaveAcc(),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          body: Stack(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.6,
+                width: MediaQuery.of(context).size.width,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Color(0xFFE72020),
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: const Radius.circular(80),
+                        bottomRight: const Radius.circular(80),
+                      )),
+                ),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                // crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  _buildLogo(),
+                  _buildContainer(),
+                ],
+              )
+            ],
+          )),
+    );
+  }
+}
